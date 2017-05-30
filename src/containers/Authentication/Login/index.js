df2638b287e94e90902f9fd8e57f1cefd32764bf
Bucky <buckyroberts@gmail.com>
2017-05-29 21:33:09 -0700

    Cleaning

--- src/containers/Authentication/Login/index.js
+++ src/containers/Authentication/Login/index.js
@@ -5,8 +5,19 @@ class Login extends Component {
     render() {
         return (
             <div className="row">
-                <div className="col-8">col-8</div>
-                <div className="col-4">col-4</div>
+                <div className="col-12">
+                    <form>
+                        <div className="form-group">
+                            <label>Email address</label>
+                            <input type="email" className="form-control" />
+                        </div>
+                        <div className="form-group">
+                            <label>Password</label>
+                            <input type="password" className="form-control" />
+                        </div>
+                        <button type="submit" className="btn btn-primary">Submit</button>
+                    </form>
+                </div>
             </div>
         );
     }

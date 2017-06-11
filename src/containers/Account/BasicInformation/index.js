7f686252e0da21c2c08e51bf02a704f30f228ecc
Bucky <buckyroberts@gmail.com>
2017-06-10 21:32:13 -0700

    Basic account page layouts

--- src/containers/Account/BasicInformation/index.js
+++ src/containers/Account/BasicInformation/index.js
@@ -4,11 +4,29 @@ import {connect} from 'react-redux';
 
 class BasicInformation extends Component {
 
-	render() {
-		return (
-			<div>Basic Information</div>
-		);
-	}
+    render() {
+        return (
+            <div className="card">
+                <div className="card-block">
+                    <form>
+                        <div className="form-group">
+                            <label>First name</label>
+                            <input type="first_name" className="form-control" id="first_name" defaultValue="Bucky"/>
+                        </div>
+                        <div className="form-group">
+                            <label>Last name</label>
+                            <input type="last_name" className="form-control" id="last_name" defaultValue="Roberts"/>
+                        </div>
+                        <div className="form-group">
+                            <label>Bio</label>
+                            <textarea className="form-control" id="exampleTextarea" rows="3"/>
+                        </div>
+                        <button type="submit" className="btn btn-primary">Submit</button>
+                    </form>
+                </div>
+            </div>
+        );
+    }
 
 }
 

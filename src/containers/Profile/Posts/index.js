26f59aecb0da56fe07e6aa09fb05bdec51339cf5
Bucky <buckyroberts@gmail.com>
2017-06-11 16:13:04 -0700

    Styling

--- src/containers/Profile/Posts/index.js
+++ src/containers/Profile/Posts/index.js
@@ -1,49 +1,25 @@
 import React, {Component} from 'react';
+import Post from '../../../components/Post';
 import './Posts.scss';
 
 
 class Posts extends Component {
 
-    renderReply() {
-        return (
-            <div className="media reply">
-                <img className="img-fluid" src="http://i.imgur.com/4EMtxHB.png"/>
-                <div className="media-body">
-                    <div className="user-name">Ashley Jones</div>
-                    This is a reply
-                </div>
-            </div>
-        );
-    }
-
-    renderPost() {
-        return (
-            <div className="card panel">
-                <div className="card-block">
-                    <div className="top">
-                        <div className="media">
-                            <img className="d-flex" src="http://i.imgur.com/o7Doie2.jpg"/>
-                            <div className="media-body">
-                                <div className="user-name">Bucky Roberts</div>
-                                This is my first post
-                            </div>
-                        </div>
-                    </div>
-                    <div className="actions">
-                        <a>Like</a> <a>Delete</a>
-                    </div>
-                    <div className="bottom">
-                        {this.renderReply()}
-                    </div>
-                </div>
-            </div>
+    renderPosts() {
+        return [1, 2].map(post =>
+            <Post
+                key={post}
+                rank={post}
+            />
         );
     }
 
     render() {
         return (
-            <div className="Posts">
-                {this.renderPost()}
+            <div className="card Posts">
+                <div className="card-block">
+                    {this.renderPosts()}
+                </div>
             </div>
         );
     }

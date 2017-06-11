91961da660d91aec652623e93a571b6b37a35185
Bucky <buckyroberts@gmail.com>
2017-06-11 16:45:40 -0700

    Working on post form

--- src/containers/Profile/Posts/index.js
+++ src/containers/Profile/Posts/index.js
@@ -16,10 +16,42 @@ class Posts extends Component {
 
     render() {
         return (
-            <div className="card Posts">
-                <div className="card-block">
-                    {this.renderPosts()}
+            <div className="Posts">
+
+                <div role="tabpanel">
+                    <ul className="nav nav-tabs" role="tablist">
+                        <li className="nav-item">
+                            <a className="nav-link active" id="text-tab" data-toggle="tab" href="#text" role="tab" aria-controls="text" aria-expanded="true">Text</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" id="photo-tab" data-toggle="tab" href="#photo" role="tab" aria-controls="photo" aria-expanded="false">Photo</a>
+                        </li>
+                    </ul>
+                    <div className="tab-content">
+                        <div role="tabpanel" className="tab-pane fade active show" id="text" aria-labelledby="text-tab" aria-expanded="true">
+                            <form>
+                                <input className="form-control" type="title" id="title" placeholder="Title" />
+                                <textarea className="form-control" id="description" rows="3"/>
+                                <button className="btn btn-primary" type="submit">Submit</button>
+                            </form>
+                        </div>
+                        <div className="tab-pane fade" id="photo" role="tabpanel" aria-labelledby="photo-tab" aria-expanded="false">
+                            <form>
+                                <div>DROPZONE HERE</div>
+                                <input className="form-control" type="title" id="title" placeholder="Title" />
+                                <textarea className="form-control" id="description" rows="3"/>
+                                <button className="btn btn-primary" type="submit">Submit</button>
+                            </form>
+                        </div>
+                    </div>
                 </div>
+
+                <div className="card">
+                    <div className="card-block">
+                        {this.renderPosts()}
+                    </div>
+                </div>
+
             </div>
         );
     }

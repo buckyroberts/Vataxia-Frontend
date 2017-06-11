f2508f19a4937759a0d163f556686a5bb3ffe920
Bucky <buckyroberts@gmail.com>
2017-06-11 15:51:42 -0700

    Working on following page

---
+++ src/components/FollowingUser/index.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import './FollowingUser.scss';
+
+
+class FollowingUser extends Component {
+
+    render() {
+        return (
+            <div className="card FollowingUser" key={this.props.id}>
+                <div className="card-block">
+                    <div className="media">
+                        <img className="d-flex mr-3" src="http://i.imgur.com/uuykYlB.png" />
+                        <div className="media-body">
+                            <a className="name">Sidney Crosby</a>
+                            <div className="details">Pittsburgh, PA</div>
+                        </div>
+                    </div>
+                    <div className="right-container">
+                        <div className="right-content">
+                            <button type="button" className="btn btn-primary">Unfollow</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+}
+
+export default connect(state => ({}))(FollowingUser);
--- src/containers/Profile/routes.js
+++ src/containers/Profile/routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Route} from 'react-router';
 import Profile from "./";
 import About from "./About";
+import Following from "./Following";
 import Photos from "./Photos";
 import Posts from "./Posts";
 
@@ -9,6 +10,7 @@ import Posts from "./Posts";
 export default (
     <Route path="/profile" component={Profile}>
         <Route path="about" component={About} />
+        <Route path="following" component={Following} />
         <Route path="photos" component={Photos} />
         <Route path="posts" component={Posts} />
     </Route>

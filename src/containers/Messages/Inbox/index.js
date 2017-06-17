432b184586c92a6ecf4d7b75826dd587970cef3a
Bucky <buckyroberts@gmail.com>
2017-06-17 13:51:33 -0700

    Working on messages

--- src/containers/Messages/Inbox/index.js
+++ src/containers/Messages/Inbox/index.js
@@ -14,7 +14,7 @@ class Inbox extends Component {
 
     renderMessages() {
         return [1, 2, 3].map(message =>
-            <div className="d-flex justify-content-start message-row">
+            <a className="d-flex justify-content-start message-row" href="#">
                 <div className="form-check">
                     <label className="form-check-label">
                         <input className="form-check-input" type="checkbox" />
@@ -23,7 +23,7 @@ class Inbox extends Component {
                 <div className="user">Bucky Roberts</div>
                 <div className="subject">Subject of the Message</div>
                 <div className="date ml-auto">6/17/2017</div>
-            </div>
+            </a>
         )
     }
 

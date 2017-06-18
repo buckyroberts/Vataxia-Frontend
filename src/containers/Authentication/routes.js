e30003d56991c24fbb351a9493e32ae88c2f3726
Bucky <buckyroberts@gmail.com>
2017-06-18 13:20:38 -0700

    Created higher order components for authenticated and unauthenticated containers

--- src/containers/Authentication/routes.js
+++ src/containers/Authentication/routes.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import {Route} from 'react-router';
 import Authentication from "../Authentication";
+import Unauthenticated from '../../containers/HOC/Unauthenticated';
 import Login from "./Login";
 import Register from "./Register";
 
 
 export default (
-	<Route path="/" component={Authentication}>
+	<Route path="/" component={Unauthenticated(Authentication)}>
 		<Route path="login" component={Login}/>
 		<Route path="register" component={Register}/>
 	</Route>

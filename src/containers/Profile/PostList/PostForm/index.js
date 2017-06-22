8938e24f76b46524b4ee09c20da29e0c9e4e13db
Bucky Roberts <buckyroberts@gmail.com>
2017-06-21 20:19:12 -0700

    Create post form working

--- src/containers/Profile/PostList/PostForm/index.js
+++ src/containers/Profile/PostList/PostForm/index.js
@@ -1,21 +1,26 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {Field, reduxForm} from 'redux-form';
+import {createPost} from '../../../../actions/posts/post/create';
 import {renderInput} from '../../../../utils/redux-form-fields';
 
 
 class PostForm extends Component {
 
 	formSubmit = data => {
-		const {dispatch} = this.props;
-		console.log(data)
+		const {activeUser, dispatch} = this.props;
+		dispatch(createPost({
+			...data,
+			user: activeUser.id
+		}));
 	};
 
 	render() {
 		const {handleSubmit} = this.props;
 		return (
 			<form onSubmit={handleSubmit(this.formSubmit)}>
-				<Field component={renderInput} label="Sample" name="sample" type="sample"/>
+				<Field component={renderInput} label="Title" name="title" type="title"/>
+				<Field component={renderInput} label="Body" name="body" type="body"/>
 				<button className="btn btn-primary" type="submit">Submit</button>
 			</form>
 		);
@@ -27,4 +32,6 @@ PostForm = reduxForm({
 	form: 'PostForm'
 })(PostForm);
 
-export default connect(state => ({}))(PostForm);
+export default connect(state => ({
+	activeUser: state.activeUser
+}))(PostForm);

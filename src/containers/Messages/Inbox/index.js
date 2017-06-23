b7c50d46be1d3a28dbf44434b5f97f03928b0694
Bucky Roberts <buckyroberts@gmail.com>
2017-06-23 15:23:14 -0700

    Starting on private messages

--- src/containers/Messages/Inbox/index.js
+++ src/containers/Messages/Inbox/index.js
@@ -16,11 +16,6 @@ class Inbox extends Component {
 	renderMessages() {
 		return [1, 2, 3].map(message =>
 			<Link className="d-flex justify-content-start message-row" to="/messages/read/1">
-				<div className="form-check">
-					<label className="form-check-label">
-						<input className="form-check-input" type="checkbox"/>
-					</label>
-				</div>
 				<div className="user">Bucky Roberts</div>
 				<div className="subject">Subject of the Message</div>
 				<div className="date ml-auto">6/17/2017</div>

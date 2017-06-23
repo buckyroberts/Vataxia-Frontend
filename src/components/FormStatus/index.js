15ccbd3c2b739e53f7f0e0b24f187308561ea609
Bucky Roberts <buckyroberts@gmail.com>
2017-06-23 14:35:17 -0700

    Adding FormStatus for reusable status messages

---
+++ src/components/FormStatus/index.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
+
+
+class FormStatus extends Component {
+
+	renderErrors = () => {
+		const {formState: {error}} = this.props;
+		if(!error) return;
+		return Object.keys(error)
+			.map(key => <div key={key}>{`${key} - ${error[key]}`}</div>);
+	};
+
+	renderSuccess = () => {
+		const {formState: {success}} = this.props;
+		if(!success) return;
+		return success;
+	};
+
+	render() {
+		return (
+			<div className="mb-2">
+				<span className="text-danger">{this.renderErrors()}</span>
+				<span className="text-success">{this.renderSuccess()}</span>
+			</div>
+		);
+	}
+
+}
+
+FormStatus.propTypes = {
+	formState: PropTypes.object.isRequired,
+};
+
+export default connect(state => ({}))(FormStatus);
--- src/forms/UpdatePasswordForm/index.js
+++ src/forms/UpdatePasswordForm/index.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {Field, reduxForm, reset} from 'redux-form';
 import {updatePassword} from '../../actions/accounts/user/authentication';
+import FormStatus from '../../components/FormStatus';
 import {renderInput} from '../../utils/redux-form-fields';
 
 
@@ -24,31 +25,17 @@ class UpdatePasswordForm extends Component {
 			})
 			.catch(error => {
 				this.setState({
-					error: error.response.data,
+					error: error.response.data || {Error: error.response.statusText},
 					success: null
 				});
 			});
-	};
-
-	renderErrors = () => {
-		const {error} = this.state;
-		if(!error) return;
-		return Object.keys(error)
-			.map(key => <div key={key}>{`${key} - ${error[key]}`}</div>);
-	};
-
-	renderSuccess = () => {
-		const {success} = this.state;
-		if(!success) return;
-		return success;
 	};
 
 	render() {
 		const {handleSubmit} = this.props;
 		return (
 			<form onSubmit={handleSubmit(this.formSubmit)}>
-				<div className="mb-2 text-danger">{this.renderErrors()}</div>
-				<div className="mb-2 text-success">{this.renderSuccess()}</div>
+				<FormStatus formState={this.state}/>
 				<Field component={renderInput} label="New password" name="password" type="password"/>
 				<button className="btn btn-primary" type="submit">Submit</button>
 			</form>

53e5c1ff9ca5a8e6e1da73161d0458f5137f55f7
Bucky Roberts <buckyroberts@gmail.com>
2017-06-23 12:05:28 -0700

    Starting on user form, going grocery shopping now

--- src/containers/Account/BasicInformation/index.js
+++ src/containers/Account/BasicInformation/index.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import UserForm from './UserForm';
+import UserForm from '../../../forms/UserForm';
 
 
 class BasicInformation extends Component {
--- src/forms/UserForm/index.js
+++ src/forms/UserForm/index.js
@@ -1,10 +1,15 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {Field, reduxForm} from 'redux-form';
-import {renderInput} from '../../../../utils/redux-form-fields';
+import {renderInput} from '../../utils/redux-form-fields';
 
 
 class UserForm extends Component {
+
+	state = {
+		error: null,
+		success: null
+	};
 
 	formSubmit = data => {
 		const {dispatch} = this.props;
@@ -15,8 +20,8 @@ class UserForm extends Component {
 		const {handleSubmit} = this.props;
 		return (
 			<form onSubmit={handleSubmit(this.formSubmit)}>
-				<Field component={renderInput} label="First name" name="first_name" type="text"/>
-				<Field component={renderInput} label="Last name" name="last_name" type="text"/>
+				<Field component={renderInput} label="First name" name="first_name"/>
+				<Field component={renderInput} label="Last name" name="last_name"/>
 				<button className="btn btn-primary" type="submit">Submit</button>
 			</form>
 		);

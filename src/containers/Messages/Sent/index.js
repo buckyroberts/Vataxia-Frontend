3a6b4df1ad5e2a8fe3796e68e720f1c3b61b13e4
Bucky Roberts <buckyroberts@gmail.com>
2017-06-23 16:37:42 -0700

    Sent messages now display properly

--- src/containers/Messages/Sent/index.js
+++ src/containers/Messages/Sent/index.js
@@ -1,15 +1,55 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import {Link} from 'react-router'
+import {getPrivateMessageList} from '../../../actions/private-messages/private-message/list';
+import {getUsersFullName} from '../../../utils/user';
 
 
 class Sent extends Component {
 
+    componentDidMount() {
+        const {dispatch} = this.props;
+        dispatch(getPrivateMessageList());
+    }
+
+    renderButtons() {
+        return (
+            <div className="d-flex justify-content-end">
+                <Link className="btn btn-danger" to="/messages/compose/1">Compose</Link>
+            </div>
+        );
+    }
+
+    renderPrivateMessages() {
+        const {activeUser, privateMessages, users} = this.props;
+        return Object.values(privateMessages)
+            .filter(privateMessage => privateMessage.sender === activeUser.id)
+            .map(privateMessage =>
+                <Link className="d-flex justify-content-start message-row" key={privateMessage.id} to={`/messages/read/${privateMessage.id}`}>
+                    <div className="user">{getUsersFullName(users, privateMessage.receiver)}</div>
+                    <div className="subject">{privateMessage.subject}</div>
+                    <div className="date ml-auto">{privateMessage.created_date}</div>
+                </Link>
+            )
+    }
+
     render() {
         return (
-            <div>Sent</div>
+            <div>
+                <div className="toolbar">
+                    {this.renderButtons()}
+                </div>
+                <div className="message-rows">
+                    {this.renderPrivateMessages()}
+                </div>
+            </div>
         );
     }
 
 }
 
-export default connect(state => ({}))(Sent);
+export default connect(state => ({
+    activeUser: state.activeUser,
+    privateMessages: state.privateMessages.data,
+    users: state.users.data
+}))(Sent);

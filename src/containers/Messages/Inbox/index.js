6170fdb65e23deff2507180ba52c64127cee0f89
Bucky Roberts <buckyroberts@gmail.com>
2017-06-23 16:03:42 -0700

    Inbox displaying list of received messages

--- src/containers/Messages/Inbox/index.js
+++ src/containers/Messages/Inbox/index.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {Link} from 'react-router'
 import {getPrivateMessageList} from '../../../actions/private-messages/private-message/list';
+import {getUsersFullName} from '../../../utils/user';
 
 
 class Inbox extends Component {
@@ -20,11 +21,14 @@ class Inbox extends Component {
     }
 
     renderPrivateMessages() {
-        return [1, 2, 3].map(privateMessage =>
-            <Link className="d-flex justify-content-start message-row" to="/messages/read/1">
-                <div className="user">Bucky Roberts</div>
-                <div className="subject">Subject of the Message</div>
-                <div className="date ml-auto">6/17/2017</div>
+        const {activeUser, privateMessages, users} = this.props;
+        return Object.values(privateMessages)
+            .filter(privateMessage => privateMessage.receiver === activeUser.id)
+            .map(privateMessage =>
+            <Link className="d-flex justify-content-start message-row" key={privateMessage.id} to={`/messages/read/${privateMessage.id}`}>
+                <div className="user">{getUsersFullName(users, privateMessage.sender)}</div>
+                <div className="subject">{privateMessage.subject}</div>
+                <div className="date ml-auto">{privateMessage.created_date}</div>
             </Link>
         )
     }
@@ -46,5 +50,6 @@ class Inbox extends Component {
 
 export default connect(state => ({
     activeUser: state.activeUser,
-    privateMessages: state.privateMessages.data
+    privateMessages: state.privateMessages.data,
+    users: state.users.data
 }))(Inbox);
--- src/utils/user.js
+++ src/utils/user.js
@@ -1,4 +1,4 @@
 export const getUsersFullName = (users, userId) => {
-	const {first_name, last_name} = users[userId];
-	return `${first_name} ${last_name}`;
+    const {first_name, last_name} = users[userId];
+    return `${first_name} ${last_name}`;
 };

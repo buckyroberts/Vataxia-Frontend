b9c2938253917e10831ee8c74441a61bf535ec06
Bucky Roberts <buckyroberts@gmail.com>
2017-06-23 17:02:24 -0700

    Cleaning

--- src/utils/user.js
+++ src/utils/user.js
@@ -1,5 +1,4 @@
 export const getUsersFullName = (users, userId) => {
-    console.log(users, userId);
     const {first_name, last_name} = users[userId];
     return `${first_name} ${last_name}`;
 };
